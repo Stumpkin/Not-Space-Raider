4c83b5eb7d46c9d230748103386fc9e127deb9be
Stumpkin <banksjalen1@gmail.com>
2020-03-09 15:43:02 -0700

    Not muchs has changed

---
+++ Assets/Scripts/Button_Do_THING.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button_Do_THING : MonoBehaviour
+{
+    public void Marv()
+    {
+        Debug.Log("AAAAAAAAAAAAAAAAAA");
+        SceneManager.LoadScene("MainG");
+    }
+}
